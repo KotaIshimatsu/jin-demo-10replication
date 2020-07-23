54588ff38dffd8b805bb7538091d867f71bfc17d
KotaIshimatsu <meerat0k.g@gmail.com>
2020-07-23 11:36:06 +0900

    即時関数を追加

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-$('.pickup-contents').slick({
-    autoplay:true,
-    autoplaySpeed:5000,
-    dots:true,
-    slidesToShow:3,
-    slidesToScroll:1  
-});
+$(function() {
+    $('.pickup-contents').slick({
+        autoplay:true,
+        autoplaySpeed:5000,
+        dots:true,
+        slidesToShow:5,
+        slidesToScroll:1  
+    });
+
+})
