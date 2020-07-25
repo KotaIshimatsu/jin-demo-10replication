24b4c7f80649383f8b57e538272082d746239b53
kuboyoichiro <kubo_yoichiro_xa@cyberagent.co.jp>
2020-07-25 16:24:01 +0900

    chore: fix slick

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 $(function() {
     $('.pickup-contents').slick({
+        arrows: true,
         autoplay:true,
         autoplaySpeed:5000,
         dots:true,
         slidesToShow:5,
-        slidesToScroll:1  
+        slidesToScroll:1,
+        variableWidth: true,
     });
 
 })
